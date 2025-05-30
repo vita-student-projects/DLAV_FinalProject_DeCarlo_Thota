bc6bbc63d1d7199fa8c9b41a8e3dc9864f667dd8
DCGius <150841296+DCGius@users.noreply.github.com>
2025-05-30 10:51:50 +0200

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -14,14 +14,19 @@ def get_sim_transform(image_size=(224, 224)):
         transforms.RandomAffine(degrees=5, translate=(0.05, 0.05), scale=(0.9, 1.1)),
         transforms.RandomPerspective(distortion_scale=0.3, p=0.3),
         transforms.RandomApply([transforms.GaussianBlur(kernel_size=3)], p=0.3),
-        transforms.RandomErasing(p=0.2, scale=(0.02, 0.15), ratio=(0.3, 3.3)),       
+        transforms.RandomErasing(p=0.2, scale=(0.02, 0.15), ratio=(0.3, 3.3)),
     ])
 
 def get_real_transform(image_size=(224, 224)):
     return transforms.Compose([
         transforms.Resize(image_size),
         transforms.RandomApply([transforms.GaussianBlur(kernel_size=3)], p=0.3),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1),
+        transforms.RandomGrayscale(p=0.2),
+        transforms.RandomAffine(degrees=5, translate=(0.05, 0.05), scale=(0.9, 1.1)),
+        transforms.RandomPerspective(distortion_scale=0.3, p=0.3),
+        transforms.RandomErasing(p=0.2, scale=(0.02, 0.15), ratio=(0.3, 3.3)),
     ])
 
 def get_test_transform(image_size=(224, 224)):
@@ -31,15 +36,18 @@ def get_test_transform(image_size=(224, 224)):
     ])
 
 class DrivingDataset(Dataset):
-    def __init__(self, file_list, is_real = False, test=False):
+    def __init__(self, file_list, is_real = False, test=False, validate=False):
         self.samples = file_list
         self.test = test
+        self.validate = validate
         self.cmd2idx = {'forward': 0, 'left': 1, 'right': 2}
 
-        if self.test:
+        if self.test or self.validate:
             self.transform = get_test_transform()
         else:
             self.transform = get_real_transform() if is_real else get_sim_transform()
+
+
 
     def __len__(self):
         return len(self.samples)
@@ -61,8 +69,10 @@ class DrivingDataset(Dataset):
         ego_flag = np.ones((21, 1))
 
         history = np.concatenate([
-            pos_xyh          # (21, 3)
-        ], axis=1)  # (21, 7)
+            pos_xyh,          # (21, 3)
+            vel,              # (21, 3)
+            acc,              # (21, 3)
+        ], axis=1)  # (21, 9)
 
         if not self.test:
             future = data['sdc_future_feature'][:, :3]
